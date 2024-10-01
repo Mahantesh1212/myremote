5dfe4844256d09b50e380ff9988c7e2d1e6bd5cd
akash <er.niharika.s.p@gmail.com>
2024-10-01 14:38:22 +0530

    uploading one.c

---
+++ one.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Print Hello, World! to the console
+    printf("Hello, World!\n");
+    return 0; // Indicate that the program ended successfully
+}
+
+    
+   
